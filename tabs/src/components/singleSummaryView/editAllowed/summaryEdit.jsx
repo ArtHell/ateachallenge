8bc73aafd38a9aea1164a43ee3f10f61d8467301
artheii <temka19977@mail.ru>
2022-06-19 08:28:57 +0200

    fix button

--- tabs/src/components/singleSummaryView/editAllowed/summaryEdit.jsx
+++ tabs/src/components/singleSummaryView/editAllowed/summaryEdit.jsx
@@ -7,6 +7,7 @@ import { AppContext } from '../../context';
 import { saveSummary } from '../../../services/summaryService';
 import { useHistory } from "react-router-dom";
 import { TeamsContext } from '../../context';
+import { Checkbox, Dropdown, Form, FormInput, FormButton, Input, Button } from '@fluentui/react-northstar';
 
 const SummaryEdit = () => {
   const [context, setContext] = useContext(AppContext);
@@ -23,7 +24,7 @@ const SummaryEdit = () => {
         <InfoCard meetingName={context.meetingName} meetingLink={context.meetingLink} meetingDate={context.meetingDate} participants={context.participants} />
         {context.summary.map(sec => <SectionCardEditable key={sec.sectionName} section={sec} />)}
         <div className='btn-right'>
-          <SharePopup saveSummary={createSummary} />
+          <SharePopup triggerButton={<Button content="Save" onClick={createSummary}/>} title={'Share'}/>
           {/* <Button primary style={{ padding: '8px', width: 'fit-content' }}>Save changes</Button> */}
         </div>
       </React.Fragment>}
